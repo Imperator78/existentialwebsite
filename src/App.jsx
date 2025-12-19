611bc2c9d9e2f22ed083258a02cb5ba1653bc5d1
Grace Hollenbach <llamaladycreates@gmail.com>
2025-12-18 20:54:01 -0500

    added like everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import './App.css'
 import Page1 from './components/Page1'
@@ -6,8 +7,11 @@ import Page3 from './components/Page3'
 import Page7 from './components/Page7'
 import Page8 from './components/Page8'
 import Page14 from './components/Page14'
+import Infinite from './components/infinite'
+import Page4 from './components/Page4'
 
 function App() {
+  // Make `Infinite` the initial page for now (index 3)
   const [currentPage, setCurrentPage] = useState(0)
   
   const pages = [
@@ -16,7 +20,9 @@ function App() {
     <Page3 key="page3" onNext={() => setCurrentPage(3)} />,
     <Page7 key="page7" onNext={() => setCurrentPage(4)} />,
     <Page8 key="page8" onNext={() => setCurrentPage(5)} />,
-    <Page14 key="page14" onNext={() => setCurrentPage(0)} />
+    <Page14 key="page14" onNext={() => setCurrentPage(3)} />,
+    <Infinite key="infinite" onNext={() => setCurrentPage(4)}/>,
+    <Page4 key="page4" onNext={() => setCurrentPage(0)}/>
   ]
 
   return (
@@ -27,3 +33,4 @@ function App() {
 }
 
 export default App
+
